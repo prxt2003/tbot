68e85d17c7e6dc8856547a72e70457fe663fd927
Prashant <prashantriyer@hotmail.com>
2020-11-13 19:10:26 +0800

    changed status

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await client.change_presence(status=discord.Status.idle, activity=discord.Game('L'))
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name='you L'))
 
 
 @client.event
